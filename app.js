4a0cc67647a9e1cd6615025b5402d5163794047c
Lita Chang <fireice47@gmail.com>
2014-10-26 15:14:35 +0800

    can read file

---
+++ app.js
@@ -0,0 +1,14 @@
+(function() {
+    var app = angular.module("app", []);
+
+    app.controller('Scanner', function() {
+        this.reference = "";
+        this.filePath = "";
+        this.fileContent = "";
+        this.setFilePath = function() {
+            this.filePath = "ans/" + this.reference + ".pdf";
+            this.reference = "";
+        };
+
+    });
+})();
